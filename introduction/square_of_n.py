06b6617b6ec8e826841ce4a6243cca737b04f3bc
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-05-28 03:10:35 +0200

    Create square_of_n.py
    if __name__ == '__main__':
        n = int(input())
    
        if n >= 0:
            for num in range(n):
                print(num*num)
    
    # The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
    
    # Example
    
    # The list of non-negative integers that are less than  is . Print the square of each number on a separate line.

---
+++ introduction/square_of_n.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    if n >= 0:
+        for num in range(n):
+            print(num*num)
+
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+
+# Example
+
+# The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
