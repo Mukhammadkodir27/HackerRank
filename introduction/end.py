c0319f19772ac2564cb25403da8e74511493cd18
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-05-28 03:32:51 +0200

    Create end.py
    for num in nums:
         print(num, end="")

---
+++ introduction/end.py
@@ -0,0 +1,11 @@
+def run(n):
+    for num in range(1, n + 1):
+        print(num, end='')
+
+
+if __name__ == '__main__':
+    n = int(input())
+    run(n)
+
+
+#! end is used to skip the new line while using for loops print
